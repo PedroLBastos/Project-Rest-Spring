fb7c9ab2109562bc5e15523b7e57f038b42043e6
Pedro Bastos <pedrolsouza10@outlook.com>
2023-02-23 15:07:06 -0300

    Repository with Hibernate e Spring Data JPA

---
+++ Projeto-Spring/Project-Rest-Spring/src/main/java/br/com/pedro/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.pedro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.pedro.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{}
