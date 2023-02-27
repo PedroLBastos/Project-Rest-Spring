9a33efe1859098f67950479aa1d5937c2d6dc200
PedroLBastos <120402542+PedroLBastos@users.noreply.github.com>
2023-02-27 15:58:24 -0300

    Design pattern VO

--- Projeto-Spring/Project-Rest-Spring/src/main/java/br/com/pedro/controllers/PersonController.java
+++ Projeto-Spring/Project-Rest-Spring/src/main/java/br/com/pedro/controllers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.pedro.model.Person;
+import br.com.pedro.data.vo.v1.PersonVO;
 import br.com.pedro.services.PersonServices;
 
 @RestController
@@ -26,27 +26,27 @@ public class PersonController {
 
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-		public List<Person> findAll() {
+		public List<PersonVO> findAll() {
 			return service.findAll();
 	}
 	
 	@GetMapping(value = "/{id}",			
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 	
 	@PostMapping(
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
+	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 	
 	@PutMapping(
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
+	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
 	
